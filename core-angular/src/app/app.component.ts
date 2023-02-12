806f3420690f903be7161cd35081774ce7c19169
Programmer Sanket <55836873+DevSanket@users.noreply.github.com>
2023-02-12 17:11:00 +0530

    Hello-World Controller is Done for backend Todo Connected

---
+++ core-angular/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector:"my-app",
+    template:`<h1>Hey Angular</h1>`,
+    styles:[`
+    h1{
+       color:red 
+    }
+    `]
+})
+export class AppComponent{
+
+}
